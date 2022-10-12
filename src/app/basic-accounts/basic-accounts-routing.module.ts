400b1e0f933ffd628a2071120b8b48a13f627f71
YelovSK <hampatrik@gmail.com>
2022-10-12 18:13:37 +0200

    aa

--- src/app/basic-accounts/basic-accounts-routing.module.ts
+++ src/app/basic-accounts/basic-accounts-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TransactionListComponent} from "./transaction-list/transaction-list.component";
+import {TransactionDetailComponent} from "./transaction-detail/transaction-detail.component";
 
 const routes: Routes = [
-  { path: '', component: TransactionListComponent},
+  { path: '', component: TransactionListComponent },
+  { path: '/detail', component: TransactionDetailComponent },
 ];
 
 @NgModule({
