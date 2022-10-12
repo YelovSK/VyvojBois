80cb056a650ed7cc9b7773b7c7cb161359c12330
YelovSK <hampatrik@gmail.com>
2022-10-12 18:12:49 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {BasicAccountsModule} from "./basic-accounts/basic-accounts.module";
 
 const routes: Routes = [
-  {
-    path: ''
-  }
+  { path: "dashboard", component: DashboardComponent},
+  { path: "basic-accounts", loadChildren: () => import('./basic-accounts/basic-accounts.module').then(m => m.BasicAccountsModule) }
 ];
 
 @NgModule({
--- src/app/basic-accounts/basic-accounts-routing.module.ts
+++ src/app/basic-accounts/basic-accounts-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TransactionListComponent} from "./transaction-list/transaction-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TransactionListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
